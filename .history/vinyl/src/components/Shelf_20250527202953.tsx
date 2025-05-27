0314f3a140110c02e05da73970834582859189ee
montel <just_alish@proton.me>
2025-05-27 20:44:27 +0500

    deleted useless files.

---
+++ .history/vinyl/src/components/Shelf_20250527202953.tsx
@@ -0,0 +1,54 @@
+import "../styles/Shelf.css";
+import Vinyl from "./Vinyl.tsx";
+<<<<<<< HEAD
+import vinyls from "../data/vinyls.json";
+import { useState, useEffect } from "react";
+=======
+
+import vinyls from "../../public/vinyls/vinyls.json";
+import { useState, useEffect } from "react";
+>>>>>>> b586c679bd3f5cabc4362d7a9cde24b03dd48733
+
+function Shelf() {
+  const shelves = [];
+  const [vinylsPerShelf, setVinylsPerShelf] = useState(() =>
+    window.innerWidth < 768 ? 2 : 4
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setVinylsPerShelf(2);
+      } else {
+        setVinylsPerShelf(4);
+      }
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  for (let i = 0; i < vinyls.length; i += vinylsPerShelf) {
+    shelves.push(vinyls.slice(i, i + vinylsPerShelf));
+  }
+
+  return (
+    <>
+      {shelves.map((group, index) => (
+        <div className="shelf" key={index}>
+          <div className="records">
+            {group.map((vinyl, i) => (
+              <Vinyl key={i} img={vinyl.image} />
+            ))}
+          </div>
+          <div className="shelf-body"></div>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default Shelf;
---
+++ .history/vinyl/src/components/Shelf_20250527203035.tsx
@@ -0,0 +1,47 @@
+import "../styles/Shelf.css";
+import Vinyl from "./Vinyl.tsx";
+import vinyls from "../data/vinyls.json";
+
+function Shelf() {
+  const shelves = [];
+  const [vinylsPerShelf, setVinylsPerShelf] = useState(() =>
+    window.innerWidth < 768 ? 2 : 4
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setVinylsPerShelf(2);
+      } else {
+        setVinylsPerShelf(4);
+      }
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  for (let i = 0; i < vinyls.length; i += vinylsPerShelf) {
+    shelves.push(vinyls.slice(i, i + vinylsPerShelf));
+  }
+
+  return (
+    <>
+      {shelves.map((group, index) => (
+        <div className="shelf" key={index}>
+          <div className="records">
+            {group.map((vinyl, i) => (
+              <Vinyl key={i} img={vinyl.image} />
+            ))}
+          </div>
+          <div className="shelf-body"></div>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default Shelf;
